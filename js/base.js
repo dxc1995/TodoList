e5fc127f38c9dcf76cc9b2367085777ec3834c4b
Your Name <email@example.com>
2018-06-18 00:45:54 +0800

    代码注释

--- js/base.js
+++ js/base.js
@@ -2,14 +2,23 @@
    "use strict";
     
     var $form_add_task = $(".add-task")
+      , $task_delete_trigger
+      , $task_delete
+      , $task_detail_trigger
       , $delete_task
-      //, new_task = {}
       , task_list = {}
+      , $task_detail = $(".task-detail")
+      , $task_detail_mask = $(".task-detail-mask")
+      , current_index
+      , $update_form
+      , $task_detail_content
+      , $task_detail_content_input
       ;
 
     init();
 
     $form_add_task.on("submit", on_add_task_form_submit);
+    $task_detail_mask.on("click", hide_task_detail);
 
     function on_add_task_form_submit(e) {
     	var new_task = {}, $input;
@@ -27,9 +36,109 @@
     	}
     }
      
+     /* 监听打开Task详情事件 */
+     function Listion_task_detail() {
+        var index;
+        $(".task-item").on("dblclick", function() {
+            index = $(this).data("index");
+            show_task_detail(index);
+        })
+
+        $task_detail_trigger.on("click", function() {
+          var $this = $(this);
+          var $item = $this.parent().parent();
+          index = $item.data("index");
+          show_task_detail(index)
+        })
+     }
+    
+     /* 查看Task详情 */
+     function show_task_detail(index) {
+        /* 生成详情模板 */
+        render_task_detail(index);
+        current_index = index;
+        /* 显示详情模板(默认隐藏) */
+        $task_detail.show();
+        /* 显示详情模板mask(默认隐藏)*/
+        $task_detail_mask.show();
+     }
+     
+     /* 更新Task */
+     function update_task(index, data) {
+        if (!index === undefined || !task_list[index]) {
+          return;
+        }
+        task_list[index] =  data;
+        refresh_task_list();
+     } 
+     
+     /* 隐藏Task详情 */
+     function hide_task_detail() {
+        $task_detail.hide();
+        $task_detail_mask.hide();
+     }
+     
+     /* 渲染指定Task的详细信息 */
+     function render_task_detail(index) {
+      if(index == undefined || !task_list[index]) {
+         return;
+      }
+
+      var item = task_list[index];
+
+      var tpl = 
+      '<form>' + 
+      '<div class="content">'+
+           item.content + 
+       '</div>'+
+         '<div class="input-item">'+
+         '<input style="display: none" type="text" name="content" value="' +  (item.content || "") + '">' +
+        '</div>'+
+      '<div>'+
+          '<div class="desc input-item">'+
+          '<textarea name="desc">' + (item.desc || "") + '</textarea>'+
+      '</div>'+
+      '</div>'+
+      '<div class="remind input-item">'+
+        '<input name="remind_date" type="date" value="' + item.remind_date +'">'+
+      '</div>'+
+         '<div class="input-item"><button type="submit">更新</button></div>' +
+      '</form>';
+      
+      /* 用新模板替换旧模板 */
+      $task_detail.html(null);
+      $task_detail.html(tpl);
+      /* 选中其中的form元素,因为之后会使用其监听submit事件*/
+      $update_form = $task_detail.find("form");
+      /* 选中显示Task内容的元素 */
+      $task_detail_content = $update_form.find(".content");
+
+      /* 选中显示Task input的元素 */
+      $task_detail_content_input = $update_form.find("[name=content]");
+      /* 双击内容元素显示input 隐藏自己 */
+      $task_detail_content.on("dblclick", function() {
+         $task_detail_content_input.show();
+         $task_detail_content.hide();
+      })
+
+      $update_form.on("submit", function(e) {
+        e.preventDefault();
+        var data = {};
+        /* 获取表单中各个input的值 */
+        data.content = $(this).find('[name=content]').val();
+        data.desc = $(this).find('[name=desc]').val();
+        data.remind_date = $(this).find('[name=remind_date]').val();
+        //console.log("data", data);
+
+        update_task(index, data);
+        hide_task_detail();
+      })
+     }
+
+
      /* 查找并监听所有删除按钮的点击事件 */
      function Listion_task_delete() {
-     	$delete_task.on("click", function () {
+     	  $task_delete_trigger.on("click", function () {
         var $this = $(this);
         /* 找到删除按钮所在的task元素 */
         var $item = $this.parent().parent();
@@ -84,8 +193,10 @@
     		$task_list.append($task);
     	}
 
-    	$delete_task = $(".action.delete");
+    	$task_delete_trigger = $(".action.delete");
+      $task_detail_trigger = $(".action.detail");
     	Listion_task_delete();
+      Listion_task_detail();
     }
     
     /* 渲染单条Task模板 */
@@ -97,7 +208,7 @@
        '<span class="task-content">'+ data.content +'</span>' + 
        '<span class="fr">'+
        '<span class="action delete"> 删除</span>' + 
-       '<span class="action"> 详细</span>' +
+       '<span class="action detail"> 详细</span>' +
        '</span>'+
        '</div>';
      return $(list_item_tpl);
