e18e99eaf828f3b7063f28164b0a541d4516aa9b
Your Name <email@example.com>
2018-06-21 01:47:27 +0800

    自定义alert完成

--- js/base.js
+++ js/base.js
@@ -2,6 +2,8 @@
    "use strict";
     
     var $form_add_task = $(".add-task")
+      , $window = $(window)
+      , $body = $("body")
       , $task_delete_trigger
       , $task_delete
       , $task_detail_trigger
@@ -24,6 +26,129 @@
 
     $form_add_task.on("submit", on_add_task_form_submit);
     $task_detail_mask.on("click", hide_task_detail);
+
+    function pop(arg) {
+      if (!arg) {
+        console.error("pop title is required");
+      }
+
+      var conf = {}
+        , $box
+        , $mask
+        , $title
+        , $content
+        , $confirm
+        , $cancel
+        , dfd
+        , timer
+        , confirmed
+        ;
+      
+      dfd = $.Deferred();
+      if (typeof arg == "string") {
+        conf.title = arg;
+      } else {
+        conf = $.extend(conf, arg);
+      }
+      
+
+      $box = $("<div>" + 
+        '<div class="pop-title">'+ conf.title +'</div>' +
+        '<div class="pop-content">' +
+        '<div><button style="margin-right: 5px" class="primary confirm">确定</button><button class="cancel">取消</button></div>'+
+        '</div>' +
+        '</div>')
+        .css({
+          color:'#444',
+          width: 300,
+          height: 'auto',
+          padding: "15px 10px",
+          background: '#fff',
+          position: 'fixed',
+          'border-radius': 3,
+          'box-shadow': '0 1px 2px rgba(0,0,0,.5)'
+        })
+
+      $title = $box.find(".pop-title").css({
+        padding: "5px 10px",
+        'font-weight': 900,
+        'font-size': 20,
+        'text-align': 'center'
+      });
+
+      $content = $box.find(".pop-content").css({
+        padding: "5px 10px",
+        'text-align': 'center'
+      })
+
+      $confirm = $content.find("button.confirm");
+      $cancel = $content.find("button.cancel");
+
+      $mask = $("<div></div>")
+         .css({
+          position: 'fixed',
+          background: 'rgba(0,0,0,.5)',
+          top: 0,
+          bottom: 0,
+          left: 0,
+          right: 0
+         })
+
+      timer = setInterval(function() {
+         if (confirmed !== undefined) {
+            dfd.resolve(confirmed);
+            clearInterval(timer);
+            dismiss_pop()
+         }
+      }, 50)
+
+      $confirm.on('click', on_confirmed);
+
+      $cancel.on('click', on_cancel);
+      $mask.on("click", on_cancel);
+
+      function on_cancel() {
+        confirmed = false;
+      }
+
+      function on_confirmed() {
+        confirmed = true;
+      }
+
+      function dismiss_pop() {
+         $mask.remove();
+         $box.remove();
+      }
+
+      function adjust_box_position() {
+         var window_width = $window.width()
+           , window_height = $window.height()
+           , box_width = $box.width()
+           , box_height = $box.height()
+           , move_x
+           , move_y
+           ;
+        move_x = (window_width - box_width) / 2;
+        move_y = (window_height - box_height) / 2 - 20;
+
+        $box.css({
+          left: move_x,
+          top: move_y
+        })
+
+      }
+
+      $window.on("resize", function() {
+        adjust_box_position();
+      })
+
+     
+
+      $mask.appendTo($body);
+      $box.appendTo($body);
+      $window.resize();
+      return dfd.promise();
+    }
 
     function listen_msg_event() {
        $msg_confirm.on("click", function() {
@@ -73,7 +198,7 @@
           if (item.complete) {
               update_task(index, {complete: false});
           } else {
-               update_task(index, {complete: true});  
+              update_task(index, {complete: true});  
           }
 
         })
@@ -178,8 +303,10 @@
         var $item = $this.parent().parent();
         var index = $item.data("index");
         /* 确认删除 */
-        var tmp = confirm("确定删除?");
-        tmp ? delete_task(index) : null;
+         pop("确定删除?")
+           .then(function(r) {
+              r ? delete_task(index) : null;
+           })
        })
      }
 
